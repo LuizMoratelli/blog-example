80fd73b189cb7e3ba8ae9f27449ed9f35fe1bd19
Luiz Moratelli <gameplayteam12@gmail.com>
2018-08-02 21:26:32 -0300

    Iniciando

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Luiz Moratelli</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/app.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <h1>LuizMoratelli</h1>
+            <img src="assets/img/logo.png" alt="logo">
+            <nav>
+                <ul>
+                    <li><a href="/Home">LuizMoratelli</a></li>
+                    <li><a href="/Tags">Tags</a></li>
+                    <li><a href="/Sobre">Sobre</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <article>
+                Post
+            </article>
+        </main>
+        <footer>
+        </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </body>
+</html>
